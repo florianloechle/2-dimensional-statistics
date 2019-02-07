f71e364feb3d1c00425d5477d9f15cd239899f22
Markus Kluge <m.kluge1@gmx.de>
2019-02-07 09:41:02 +0100

    feat: first iteration of table input

--- src/components/ContingencyTable.js
+++ src/components/ContingencyTable.js
@@ -6,50 +6,97 @@ export default class ContingencyTable extends React.Component {
   state = {
     labelHor: 'Age',
     labelVer: 'Income',
-    xValues: [1, 2, 3],
-    yValues: [2, 3, 4],
-    xyValues: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    xThead: [1, 2, 3],
+    yTHead: [2, 3, 4],
+    values: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
   };
 
+  addHorizontal() {
+    this.setState(state => {
+      const xThead = state.xThead;
+      const newXY = this.state.values.map(a => [...a, 0]);
+
+      return {
+        xThead: [...xThead, 0],
+        values: newXY,
+      };
+    });
+  }
+
+  addVertical() {
+    this.setState(state => {
+      return {
+        yTHead: [...state.yTHead, 0],
+        values: [...state.values, new Array(state.xThead.length).fill(0)],
+      };
+    });
+  }
+
+  changeVal(event, index) {
+    console.log(event);
+    this.setState(state => {
+      const newX = state.xThead;
+      newX[index] = event.target.value;
+
+      return {
+        xThead: newX,
+      };
+    });
+  }
+
   renderFirstRow() {
-    const td = this.state.xValues.map(value => (
-      <td className="tdBold">{value}</td>
+    const td = this.state.xThead.map((value, i) => (
+      <th key={i} className="tdBold">
+        <input
+          onChange={e => this.changeVal(e, i)}
+          type="number"
+          value={value}
+        />
+      </th>
     ));
     return td;
   }
 
   renderRest() {
-    var row = 0;
-    const rows = this.state.yValues.map(yValue => (
-      <tr>
-        <td className="tdBold">{yValue}</td>
-        {this.state.xyValues[row].map(
-          arrayDim2 => (
-            <td>{arrayDim2}</td>
-          ),
-          (row += 1)
-        )}
+    const rows = this.state.yTHead.map((yTHead, index) => (
+      <tr key={index}>
+        <td className="tdBold">
+          <input type="number" />
+        </td>
+        {this.state.values[index].map((arrayDim2, i) => (
+          <td key={i}>
+            <input type="number" />
+          </td>
+        ))}
       </tr>
     ));
 
     return rows;
   }
 
-  renderTable() {
-    return (
-      <table>
-        <tr>
-          <td className="tdBold">
-            {this.state.labelHor}/{this.state.labelVer}
-          </td>
-          {this.renderFirstRow()}
-        </tr>
-        {this.renderRest()}
-      </table>
-    );
-  }
-
   render() {
-    return <div>{this.renderTable()}</div>;
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <td className="tdBold">
+                {this.state.labelHor} / {this.state.labelVer}
+              </td>
+              {this.renderFirstRow()}
+              <td className="buttonAdd" onClick={this.addHorizontal.bind(this)}>
+                +
+              </td>
+            </tr>
+            {this.renderRest()}
+            <tr>
+              <td className="buttonAdd" onClick={this.addVertical.bind(this)}>
+                +
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
