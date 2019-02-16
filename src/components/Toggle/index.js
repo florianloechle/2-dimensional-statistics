2a662f9d19e6e2425e7373761490be33f9e74ed1
Florian Loechle <loechle.florian@gmail.com>
2019-02-16 14:39:05 +0100

    wip: cleanup, add toggle component

---
+++ src/components/Toggle/index.js
@@ -0,0 +1,58 @@
+/** @format */
+
+import React from 'react';
+import styles from './Toggle.module.css';
+
+class SelectionToggle extends React.Component {
+  state = {
+    active: false,
+  };
+
+  onChange = newState => {
+    this.setState(
+      {
+        active: newState,
+      },
+      () => this.props.onChange(this.state.active)
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <span className="custom-control custom-radio">
+          <input
+            type="radio"
+            checked={this.state.active === false}
+            id="checkDotSequence"
+            name="radioInput"
+            className="custom-control-input"
+            onChange={_ => this.onChange(false)}
+          />
+          <label className="custom-control-label" htmlFor="checkDotSequence">
+            Punktfolge
+          </label>
+        </span>
+
+        <span className="custom-control custom-radio">
+          <input
+            type="radio"
+            checked={this.state.active}
+            id="checkContigencyTable"
+            name="radioInput"
+            className="custom-control-input"
+            onClick={_ => this.onChange(true)}
+          />
+          <label
+            className="custom-control-label"
+            htmlFor="checkContigencyTable"
+          >
+            Kontigenztafel
+          </label>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default SelectionToggle;
