32ea9e38903e08cf0b04174da81e4a7691b4cf6f
Markus Kluge <m.kluge1@gmx.de>
2019-02-16 12:37:41 +0100

    chore: cleanup after merge

--- src/App.js
+++ src/App.js
@@ -4,25 +4,100 @@ import React from 'react';
 import Layout from './components/Layout';
 import DotSequency from './components/DotSequency';
 import styles from './App.module.css';
-import Statistic from './lib/Statistics';
-import TextInput from './components/Input/Text';
 import ContingencyTable from './components/ContingencyTable';
+// import Statistic from './lib/Statistics';
+// import TextInput from './components/Input/Text';
 
 class StatisticApp extends React.Component {
   state = {
     statistics: null,
+    userChoice: null,
+    //userChoice: 2
   };
+
+  setUserChoice(value) {
+    this.setState({ userChoice: value });
+  }
+
+  renderChoiceRadioButtons() {
+    return (
+      <div>
+        <div>
+          <div className="card">
+            <div className="card-body">
+              <div className="custom-control custom-radio">
+                <input
+                  type="radio"
+                  id="checkDotSequence"
+                  name="radioInput"
+                  className="custom-control-input"
+                  value="2"
+                  onClick={this.setUserChoice.bind(this, 1)}
+                />
+                <label className="custom-control-label" for="checkDotSequence">
+                  Punktfolge
+                </label>
+              </div>
+
+              <div className="custom-control custom-radio">
+                <input
+                  type="radio"
+                  id="checkContigencyTable"
+                  name="radioInput"
+                  className="custom-control-input"
+                  value="1"
+                  onClick={this.setUserChoice.bind(this, 2)}
+                />
+                <label
+                  className="custom-control-label"
+                  for="checkContigencyTable"
+                >
+                  Kontigenztafel
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderChoice() {
+    if (this.state.userChoice === null) {
+      return this.renderChoiceRadioButtons();
+    } else if (this.state.userChoice === 1) {
+      return (
+        <div>
+          <DotSequency />
+        </div>
+      );
+    } else if (this.state.userChoice === 2) {
+      return (
+        <div>
+          <ContingencyTable />
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <Layout>
-        <div className={styles.sheet}>
-          <span>{new Date().toLocaleString()}</span>
-          <ContingencyTable />
-        </div>
+        <div className={styles.centerParent}>{this.renderChoice()}</div>
       </Layout>
     );
   }
+
+  // render() {
+  //   return (
+  //     <Layout>
+  //       <div className={styles.sheet}>
+  //         <span>{new Date().toLocaleString()}</span>
+  //         <ContingencyTable />
+  //       </div>
+  //     </Layout>
+  //   );
+  // }
 }
 
 export default StatisticApp;
